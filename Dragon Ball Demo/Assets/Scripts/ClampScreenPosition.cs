b5e4aec43bf6c8f708f85203c86bfffc9003a3d5
Mikabrytu <mikabrytu@gmail.com>
2020-08-27 03:31:10 -0300

    Fixing Kintoun movement screen clamp

---
+++ Dragon Ball Demo/Assets/Scripts/ClampScreenPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampScreenPosition : MonoBehaviour
+{
+    [SerializeField] private Vector4 limits;
+
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, limits.x, limits.y);
+        pos.y = Mathf.Clamp(pos.y, limits.z, limits.w);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
